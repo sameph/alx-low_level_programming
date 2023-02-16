309ae2698e3eff586ffed2bfb632cec0fddd8ac5
sameph <samuelephrem2012@gmail.com>
2023-02-16 14:36:01 -0800

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main -  this is a progrM THt prints  a line
+ *Return: -return 0 for success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
