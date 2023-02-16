e841d8e3da90d70e51db03ff1cf1a004ce3aaa89
sameph <samuelephrem2012@gmail.com>
2023-02-16 14:53:27 -0800

    second c code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - a c program to print a piece of words
+ * Return: return 0 if noerrors
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
