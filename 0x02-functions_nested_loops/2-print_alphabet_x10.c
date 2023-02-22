6a20298c716771bc8a47b6f36647455ec700caac
sameph <samuelephrem2012@gmail.com>
2023-02-22 13:55:05 -0800

    printing small letters 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printing 10 times alphabet in small letter
+ * Return: return (0) for success
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char j = 'a';
+
+	while (i < 10)
+	{
+
+	while (j < 'z')
+	{
+
+	_putchar(j);
+
+	j++;
+
+	}
+
+	_putchar("\n");
+
+	i++;
+
+	}
+
+	return (0);
+}
