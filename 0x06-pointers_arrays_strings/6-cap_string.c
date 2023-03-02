152dcd3960da6f6e531a85e09e99d2d112580773
sameph <samuelephrem2012@gmail.com>
2023-03-02 11:49:24 -0800

    capitilize the string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,34 @@
 #include "main.h"
+
 /**
- * cap_string(char *str) - function to capitilize a string
- * @str: the string to be capitilized
- * Return: return str
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
  */
-char *cap_string(char *str)
+char *cap_string(char *s)
 {
 	int i, j;
-	char c[] = {' ', '\t', '\n', ';', ',', '.', '!', '?', '\"', '(', ')', '{', '}'};
-	
 
-       while (str[i] != '\0')
-       {	       
-	       if (i == 0 && str[i] > 'a' && str[i] < 'z')
-	       {
-		       str[i] -= str[i];
-	       }
-	       
-	       j = 0;
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
-	       while (c[j] != '\0')
-	       {
-		       if (str[i] ==  c[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
-		       {
-			       str[i + 1] -= 32;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
 
-		       }
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
 
-		       j++;
-	       }
-
-	       i++;
-       }
-
+	return (s);
+}
