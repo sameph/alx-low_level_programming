1a04f7275913c274d5c8e02a37b0630d65a48877
sameph <samuelephrem2012@gmail.com>
2023-03-06 04:59:22 -0800

    a function to cop ymemory to another

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,19 @@
 #include "main.h"
 #include <string.h>
 /**
- * _memcpy: a function which copy memory
+ * *_memcpy - copies memory area
  * @dest: address where memory will be copied to
  * @src: address from where memory is copied
+ * @n: size of bytes to be copied
  * Return: character
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int i = 0;
-	while(i < n)
+
+	while (i < n)
 	{
 		dest[i] = src[i];
-		
 		i++;
 	}
 return (dest);
