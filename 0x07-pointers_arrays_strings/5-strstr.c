1428ca07db36fe88274d017cfd5a81b1dd2be600
sameph <samuelephrem2012@gmail.com>
2023-03-06 12:05:17 -0800

    matching string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strstr - a function that finds a string match
+ * @haystack: a pointer to the main string
+ * @needle: a string to be matched
+ * Return: return a pointer to char
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	needle = strstr(haystack, needle);
+	return (needle);
+}
