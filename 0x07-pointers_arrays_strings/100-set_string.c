b50d9efe5fafa4a047887f2b5863d3a955debfbc
sameph <samuelephrem2012@gmail.com>
2023-03-07 11:29:43 -0800

    a function which set the value of string to other

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - a function which sets the value of string to another
+ * @s: a string to be set
+ * @to: a string to set
+ * Return: return nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
