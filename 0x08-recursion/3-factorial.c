1f1ddf2a8639855e4402bbf201e90f3fde555f3c
sameph <samuelephrem2012@gmail.com>
2023-03-08 11:37:00 -0800

    computing factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * factorial - a function which prints factorial of given number
+ * @n: anumber whose factorial will be printed
+ * Return: return integer
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
