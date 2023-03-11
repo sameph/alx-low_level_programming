90c4265ba3837d7836cb50b4797675c9e9ab5cee
sameph <samuelephrem2012@gmail.com>
2023-03-10 22:57:57 -0800

    fixed bug

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,15 +1,21 @@
-#include <string.h>
 #include "main.h"
-/**
- * _memset - a function that fills memory with a constant byte
- * @s: is a string that points to n
- * @b: a constant byte
- * @n: the first bytes of memory area
- * Return: return character
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	char *mem = memset(s, b, n);
 
-	return (mem);
+/**
+ * *_memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
 }
