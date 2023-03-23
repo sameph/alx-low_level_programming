da3a9ffe286eb84af55855c28c7f03e81c8f296b
sameph <samuelephrem2012@gmail.com>
2023-03-23 05:22:38 -0700

    a program that searchs for integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "function_pointers.h"#include "function_pointers.h"
+#include "function_pointers.h"
 /**
  * int_index - searches for an integer
  * @array: array with data
