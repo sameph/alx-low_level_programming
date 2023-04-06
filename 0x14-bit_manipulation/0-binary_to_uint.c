1b68ee2e7bb18ee239cc9eeaeed3c995a60b7eb3
sameph <samuelephrem2012@gmail.com>
2023-04-06 13:04:15 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * binary_to_uint - a function which convert binary to decimal
+ * @b: a binary number to be converted
+ *
+ * Return: return the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	if (b == NULL)
+		return (0);
+	int k;
+	unsigned int conv = 0;
+
+	for (k = 0; b[k]; k++)
+	{
+		if (b[k] != '0' || b[k] != '1')
+			return (1);
+		conv = 2 * conv + (b[k] - '0');
+	}
+	return (conv);
+}
